31909905bedc2209a3f386a54387150b3d029e7e
Yannis <yannismosca@gmail.com>
2025-01-21 16:19:28 +0100

    Push

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    i = len(my_list) - 1
+    i = len(my_list)
 
-    while i >= 0:
-        print(my_list[i])
+    while i > 0:
+        print("{:d}".format(i))
         i -= 1
