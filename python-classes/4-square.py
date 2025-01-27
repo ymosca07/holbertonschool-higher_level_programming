100bd9f1063761e69812cc1a87e253a3ecd46581
Yannis <yannismosca@gmail.com>
2025-01-27 12:53:06 +0100

    Push

---
+++ python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+class Square:
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter = accéder valeurt attribut privé"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter = modifier valeur attribut privé en s'assurant qu'elle est valide"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self): 
+        """Retourne l'aire du carré"""
+        return self.__size * self.__size
+    
---
+++ python-classes/6-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+class Square:
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter = accéder valeurt attribut privé"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter = modifier valeur attribut privé en s'assurant qu'elle est valide"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self): 
+        """Retourne l'aire du carré"""
+        return self.__size * self.__size
+
+    def my_print(self):
+
+        c = 0
+
+        if self.__size == 0:
+            print("")
+        else:
+            while c < self.__size:
+                l = 0
+                while l < self.__size:
+                    print("#", end="")
+                    l += 1
+                print("")
+                c += 1
