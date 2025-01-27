4f8da594dd8a849a201ea7617605639924395c03
Yannis <yannismosca@gmail.com>
2025-01-27 13:05:26 +0100

    Push

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 
 class Square:
+    """
+    A class that defines a square.
 
-    """kjdddddddddddddddddddd
-    dddddddddddddddddddddd
-    ddddddddddddddddddddddddddd
-    ddddddddddddddddddddddddd
-    ddddddddddddddddddddddd
-    ddddddddddddddddddd"""
+    This class includes methods to initialize the square,
+    retrieve its size,
+    set its size with proper validation, calculate
+    its area, and print the square.
+    """
 
     def __init__(self):
+        """
+        A class that defines a square.
+
+        This class includes methods to initialize
+        the square, retrieve its size,
+        set its size with proper validation,
+        calculate its area, and print the square.
+        """
         pass
