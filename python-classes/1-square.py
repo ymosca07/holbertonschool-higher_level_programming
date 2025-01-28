a0647f1e829f828282cdcac691cfbbf331df66b1
Yannis <yannismosca@gmail.com>
2025-01-28 11:10:36 +0100

    Push

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-"""program that create the class Square and attribute size"""
+"""
+program that create the class Square and attribute size
+"""
+
+
 class Square:
-    """size is a private attribute of class square"""
+    """
+    size is a private attribute of class square
+    """
     def __init__(self, size):
         self.__size = size
