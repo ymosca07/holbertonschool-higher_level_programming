ffd10b90558c090f68a10f48fa5a2f6652c8afd3
Yannis <yannismosca@gmail.com>
2025-01-28 11:17:54 +0100

    Push

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+    A class that defines a square.
+"""
+
 
 class Square:
-
+    """
+        A class that defines a square.
+    """
     def __init__(self, size=0):
         self.__size = size
 
