57cc8d186977f2ae5e64eaec746e27d6c4f26212
Yannis <yannismosca@gmail.com>
2025-01-28 11:02:20 +0100

    Push

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 
 class Square:
-    """A class that defines a square."""
+    """
+    A class that defines a square.
+
+    This class serves as a blueprint for creating square objects,
+    which can later include attributes such as size or methods
+    to calculate the area and perimeter.
+    """
+
     def __init__(self):
-        """A def that defines a init."""
+        """
+        Initialize a new Square instance.
+
+        Currently, this constructor does not accept any parameters
+        or set any attributes, but it can be extended in the future.
+        """
         pass
--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -36,6 +36,7 @@ class Square:
         en s'assurant qu'elle est valide"""
         if not isinstance(value, int):
             raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def my_print(self):
 
