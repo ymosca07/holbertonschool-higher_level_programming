5fae42aa17d20c0a66bd3673e85581c35c2b5c8f
Yannis <yannismosca@gmail.com>
2025-01-29 10:54:39 +0100

    Push

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""J'avoue j'suis dans l'exces pour des ptits
+trajets jsors le féfé"""
+
+
+class Rectangle:
+    """J'avoue j'suis dans l'exces pour des ptits
+    trajets jsors le féfé"""
+    def __init__(self):
+        pass
