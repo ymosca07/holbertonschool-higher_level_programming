3d4fddb014c6611862f44ea31e9cb7854c9d2b61
Yannis <yannismosca@gmail.com>
2025-01-29 12:34:36 +0100

    Push

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -42,3 +42,8 @@ class Rectangle:
             return 0
         else:
             return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join("#" * self.__width for _ in range(self.__height))
