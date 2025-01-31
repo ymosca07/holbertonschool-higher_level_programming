1c3261dd3c528426832283be014113f2433842d0
Yannis <yannismosca@gmail.com>
2025-01-31 10:45:39 +0100

    Push

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -48,18 +48,18 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.position = value
+        self.__position = value
 
     def area(self):
         """ Return the area of the square """
-        return self.size ** 2
+        return self.__size ** 2
 
     def my_print(self):
         """ Print the square """
-        if self.size == 0:
+        if self.__size == 0:
             print()
             return
-        for i in range(self.position[1]):
+        for i in range(self.__position[1]):
             print()
-        for j in range(self.size):
-            print(" " * self.position[0] + '#' * self.__size)
+        for j in range(self.__size):
+            print(" " * self.__position[0] + '#' * self.__size)
