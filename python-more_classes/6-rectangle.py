82c12c12d9e0e0b057df5fe12df6b238dd339a07
Yannis <yannismosca@gmail.com>
2025-01-31 11:39:08 +0100

    Push

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -7,9 +7,12 @@ class Rectangle:
     """J'avoue j'suis dans l'exces pour des ptits
     trajets jsors le féfé"""
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -56,3 +59,4 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
