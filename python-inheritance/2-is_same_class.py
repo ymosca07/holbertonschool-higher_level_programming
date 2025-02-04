824f4bb9a82335821fb38eefeed94cc672139d76
Yannis <yannismosca@gmail.com>
2025-02-04 11:05:05 +0100

    Push

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Ceci est une description"""
+
+
+def is_same_class(obj, a_class):
+    """Ceci est une description"""
+    return type(obj) is a_class
---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Ceci est une desc"""
+
+
+def is_kind_of_class(obj, a_class):
+
+    if isinstance(obj, a_class) == 1:
+        return True
+    else:
+        return False
