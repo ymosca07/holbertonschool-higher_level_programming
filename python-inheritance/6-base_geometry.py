309d8596ae0ddd27e9969a64a2c9bcbb120585e5
Yannis <yannismosca@gmail.com>
2025-02-04 11:25:51 +0100

    Push

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Ceci est une description"""
+
+
+class BaseGeometry:
+    """Ceci est une description"""
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
