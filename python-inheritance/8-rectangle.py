84a422ffa38c1961eef578e947aab17f515e560b
Yannis <yannismosca@gmail.com>
2025-02-05 11:34:24 +0100

    Push

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,10 +4,6 @@
 
 class BaseGeometry:
     """Ceci est une description"""
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-
     def area(self):
         raise Exception("area() is not implemented")
 
@@ -16,3 +12,11 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """Ceci est une description"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
