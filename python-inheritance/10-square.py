0fa65d9ea06d1bc61670cc984ea91e0f31543e42
Yannis <yannismosca@gmail.com>
2025-02-05 11:47:23 +0100

    Push

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Ceci est une description"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Ceci est une description"""
+
+    def __init__(self, size):
+        """Initialise un carré avec `size`, validé par integer_validator."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Retourne l'aire du carré."""
+        return self.__size * self.__size
