c16da74703ea4fcfec5b2a786f1f49516b2d6f80
Yannis <yannismosca@gmail.com>
2025-02-07 12:41:49 +0100

    Push

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
-"""Ceci est une description"""
+"""
+This module implements the basegeometry class
+"""
 
 
 class BaseGeometry:
-    """Ceci est une description"""
+    """
+    This class is empty for now
+    """
+    def __init__(self):
+        """
+        Does nothing for now
+        """
+        pass
+
     def area(self):
+        """
+        Raises an error for now due to lack of implementation
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if isinstance(value, int) == 0:
+        """
+        This method will validate value if conditions are respected
+        """
+        if isinstance(value, bool):
             raise TypeError(f"{name} must be an integer")
-        if value <= 0:
+        elif not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
             raise ValueError(f"{name} must be greater than 0")
