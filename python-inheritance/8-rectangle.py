375332159da430a0a2292ed2572506c0f25a5bb5
Yannis <yannismosca@gmail.com>
2025-02-07 12:42:35 +0100

    Push

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""Ceci est une description"""
+"""
+This module implements the Rectangle class that
+inherits from BaseGeometry
+"""
 
-
-class BaseGeometry:
-    """Ceci est une description"""
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+Here we import the BaseGeometry module to use it's attributes
+"""
 
 
 class Rectangle(BaseGeometry):
-    """Ceci est une description"""
+    """
+    This class inherits from BaseGeometry
+    """
     def __init__(self, width, height):
+        """
+        This method initializes an instance from a class
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
