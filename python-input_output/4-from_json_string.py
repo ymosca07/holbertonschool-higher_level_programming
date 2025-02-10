14351f3c280a4865d559c774fd399faf26b9ab4b
Yannis <yannismosca@gmail.com>
2025-02-10 11:16:27 +0100

    Push

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Ceci est une description"""
+import json
+
+
+def from_json_string(my_str):
+    """Ceci est une description"""
+    this = json.loads(my_str)
+    return this
