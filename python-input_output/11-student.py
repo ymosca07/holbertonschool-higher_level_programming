14c12e80ab596895290d2068288c77990198e855
Yannis <yannismosca@gmail.com>
2025-02-10 14:16:04 +0100

    Push

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""Définition de la classe Student"""
+"""Ceci est une desc"""
 
 
 class Student:
-    """Classe Student avec sérialisation et désérialisation"""
+    """Ceci est une desc"""
     def __init__(self, first_name, last_name, age):
-        """Initialisation des attributs de l'étudiant"""
+        """Ceci est une desc"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retourne un dictionnaire des attributs de l'instance"""
+        """Ceci est une desc"""
         if isinstance(attrs, list):
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
         return self.__dict__
 
     def reload_from_json(self, json):
-        """Remplace les attributs de l'instance à partir d'un dictionnaire JSON"""
+        """Ceci est une desc"""
         for key, value in json.items():
             setattr(self, key, value)
