425110841b25e0615e175c2ead0dd6cb1dae8c55
Yannis <yannismosca@gmail.com>
2025-02-10 10:30:00 +0100

    Push

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """Defines a function that reads text file"""
 def read_file(filename=""):
+    """Open, read and print the contents of a file to stdout"""
     with open(filename, "r", encoding="utf-8") as file:
-         print(file.read(), end="")
+        text_file = file.read()
+        print("{}".format(text_file), end="")
+    return text_file
