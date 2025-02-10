1c1a6a792d445f4dcda5ece8d654e7c02303fbb8
Yannis <yannismosca@gmail.com>
2025-02-10 12:50:35 +0100

    Push

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+import json
+
+
+def load_from_json_file(filename):
+    """Ceci est une description"""
+    with open(filename, 'r', encoding="utf-8") as file:
+        return json.load(file)
+
+def save_to_json_file(my_obj, filename):
+    """Ceci est une descriptin"""
+    with open(filename, "w", encoding="utf-8") as file:
+        this = file.write(json.dumps(my_obj))
+    return this
+
+filename = "add_item.json"
+
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+
+items.extend(sys.argv[1:])
+
+save_to_json_file(items, filename)
