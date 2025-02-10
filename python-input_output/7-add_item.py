977246b3e3c78ae7e19f353629089ed1a35c10fa
Yannis <yannismosca@gmail.com>
2025-02-10 13:09:18 +0100

    Push

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,11 +9,13 @@ def load_from_json_file(filename):
     with open(filename, 'r', encoding="utf-8") as file:
         return json.load(file)
 
+
 def save_to_json_file(my_obj, filename):
     """Ceci est une descriptin"""
     with open(filename, "w", encoding="utf-8") as file:
         this = file.write(json.dumps(my_obj))
     return this
+
 
 filename = "add_item.json"
 
---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Ceci est une description"""
+
+
+def class_to_json(obj):
+    """Ceci est une description"""
+    return obj.__dict__
