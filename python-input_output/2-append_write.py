f3cb7abb1846756895212df2c1b423825e5a90ee
Yannis <yannismosca@gmail.com>
2025-02-10 10:54:04 +0100

    Push

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Ceci est une description"""
+
+
+def append_write(filename="", text=""):
+    """Tghifjfngfgpkg"""
+    with open(filename, "a", encoding="utf-8") as file:
+        this = file.write(text)
+    return this
