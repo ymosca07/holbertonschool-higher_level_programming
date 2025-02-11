d4d64116fa9806e9dda2d62e8ab95e6d0469f369
Yannis <yannismosca@gmail.com>
2025-02-11 12:45:47 +0100

    Push

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -6,22 +6,26 @@ import pickle
 class CustomObject:
     """Ceci est une classe"""
     def __init__(self, name, age, is_student):
+        """Ceci est une description"""
         self.name = name
         self.age = age
         self.is_student = is_student
-    
+
     def display(self):
+        """Ceci est une description"""
         print(f"Name: {self.name}")
         print(f"Age: {self.age}")
         print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
+        """Ceci est une description"""
         with open(filename, "wb") as file:
             pickle.dump(self, file)
         return filename
 
-    @classmethod 
+    @classmethod
     def deserialize(cls, filename):
+        """Ceci est une description"""
         with open(filename, "rb") as file:
             cls = pickle.load(file)
         return cls
