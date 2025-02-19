e7db0e81d062360da8ccc9aa73e564b53d03daf7
Yannis <yannismosca@gmail.com>
2025-02-19 11:13:17 +0100

    Push

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,13 +1,22 @@
+#!/usr/bin/python3
+"""
+This modules defines a subclass of BaseHTTPRequestHandler
+"""
+import http.server
+import socketserver
 import json
-from http.server import BaseHTTPRequestHandler, HTTPServer
-"""Ceci est une description"""
 
 
-class SimpleAPIHandler(BaseHTTPRequestHandler):
-    """Ceci est une description"""
-
+class http_SubClass(http.server.BaseHTTPRequestHandler):
+    """
+    This subclass implement a method to handle GET requests.
+    """
     def do_GET(self):
-        """Ceci est une description"""
+        """
+        This methods is used to handle GET requests when accessing
+        'data', 'info' and 'status' endpoint.
+        And returns a 404 Not Found status for undefined endpoint.
+        """
         if self.path == "/":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
@@ -15,45 +24,33 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self.wfile.write(b"Hello, this is a simple API!")
 
         elif self.path == "/data":
+            dataset = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
+            self.wfile.write(json.dumps(dataset).encode("utf-8"))
 
-            data = {"name": "John", "age": 30, "city": "New York"}
-            self.wfile.write(json.dumps(data).encode("utf-8"))
+        elif self.path == "/info":
+            dataset = {"version": "1.0", "description":
+                       "A simple API built with http.server"}
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(dataset).encode("utf-8"))
 
         elif self.path == "/status":
             self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps({"status": "OK"}).encode("utf-8"))
-
-        else:
-            self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"Endpoint not found")
-
-    def do_POST(self):
-        """Ceci est une description"""
-        if self.path == "/message":
-            content_length = int(self.headers.get("Content-Length", 0))
-            post_data = self.rfile.read(content_length).decode("utf-8")
-
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-
-            response = {"message": "Received", "data": post_data}
-            self.wfile.write(json.dumps(response).encode("utf-8"))
+            self.wfile.write(b"OK")
         else:
             self.send_response(404)
-            self.send_header("Content-type", "text/plain")
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"Endpoint not found")
 
 
-server_address = ("", 8000)
-httpd = HTTPServer(server_address, SimpleAPIHandler)
-print("Server running on port 8000...")
-httpd.serve_forever()
+PORT = 8000
+with socketserver.TCPServer(("", PORT), http_SubClass) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
