ad851c26c8319762f635a3ae5068acf8c8fafe75
Yannis <yannismosca@gmail.com>
2025-03-05 13:04:07 +0100

    Push

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- Description
+SELECT tv_shows.title, tv_show_genres.id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
