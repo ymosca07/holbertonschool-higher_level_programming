7606175d3640ba55555a0c1a7ed97dfe50eeb628
Yannis <yannismosca@gmail.com>
2025-03-06 12:17:49 +0100

    Push

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- Description
+SELECT tv_genres.name
+FROM tv_genres
+RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS 'Dexter'
+ORDER BY tv_genres.name ASC;
