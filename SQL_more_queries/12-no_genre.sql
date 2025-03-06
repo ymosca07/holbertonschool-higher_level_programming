3f321d12a6c40d7b9b909ebad74320244501d7fa
Yannis <yannismosca@gmail.com>
2025-03-06 11:46:12 +0100

    Push

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- Description
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_show_id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
