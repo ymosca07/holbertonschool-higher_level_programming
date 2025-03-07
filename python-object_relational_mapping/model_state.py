24c35d42a12403aa324d0af5c248f596ddeb67e8
Yannis <yannismosca@gmail.com>
2025-03-07 14:23:42 +0100

    Push

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """Class State mapping to 'states' table"""
     __tablename__ = 'states'
