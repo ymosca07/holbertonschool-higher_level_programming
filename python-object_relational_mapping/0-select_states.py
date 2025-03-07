60a1fffca7f1e0a58ba8d6d31327dbedce0d253b
Yannis <yannismosca@gmail.com>
2025-03-07 11:05:13 +0100

    Push

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa"""
+"""Description"""
 import sys
 import MySQLdb
 
@@ -8,15 +8,16 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],   # Nom d'utilisateur MySQL
-        passwd=sys.argv[2], # Mot de passe MySQL
-        db=sys.argv[3],     # Nom de la base de données
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
     )
 
     # Création d'un curseur pour exécuter les requêtes SQL
     cur = db.cursor()
 
-    # Exécution de la requête SQL pour récupérer tous les états triés par ID croissant
+    # Exécution de la requête SQL pour récupérer
+    # tous les états triés par ID croissant
     cur.execute("SELECT id, name FROM states ORDER BY id ASC")
 
     # Affichage des résultats ligne par ligne
