4e48ecaa7e3528e96d7dec45b8671c67f3649413
Yannis <yannismosca@gmail.com>
2025-03-07 12:26:07 +0100

    Push

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-"""Description"""
-import sys
+"""
+Script qui liste tous les États de la base de données hbtn_0e_0_usa.
+"""
 import MySQLdb
-
+import sys
 if __name__ == "__main__":
-    # Connexion à MySQL avec les arguments passés en ligne de commande
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
+        db=sys.argv[3]
     )
-
-    # Création d'un curseur pour exécuter les requêtes
     cur = db.cursor()
-
-    # Exécution de la requête SQL 
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Affichage des résultats ligne par ligne
     for row in cur.fetchall():
         print(row)
-
-    # Fermeture du curseur et de la connexion
     cur.close()
     db.close()
+    
